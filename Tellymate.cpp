3d30bbc5a373d7ba1793d6c1fdf2b1fefa4ee3df
goldfish <bigryeguy@gmail.com>
2009-06-06 22:26:14 -0700

    Initial commit. Base for Tellymate library

---
+++ Tellymate.cpp
@@ -0,0 +1,93 @@
+/*
+  Tellymate.h - Arduino Library for using the Tellymate Shield
+  Created by goldfish yo! 
+  Copyright (c) 2009, goldfish yo!
+  
+  Portions based on Tellymate demo code and specifications
+  Created and Copyright (c) 2009, Nigel Batten.
+  http://www.batsocks.co.uk/
+  
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+	1.	Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+	2.	Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+===============================================
+
+TellyMate Serial to Video adapter
+=================================
+	38x25 character display
+	PAL or NTSC composite video output
+	Up to 57k6 serial data.
+	Autobauding option.
+	VT52 escape codes.
+	H19 escape codes.
+	Double-height / Double-width text.
+	Code Page 437 graphics
+	White text on black background
+*/
+
+#include "WProgram.h"
+#include "Tellymate.h"
+
+//#define CHAR_ESC "\x1B"
+
+Tellymate::Tellymate( int powerOn )
+{
+  digitalWrite( 13, powerOn );
+}
+
+void Tellymate::init( long serialRate )
+{
+  _serialRate = serialRate;
+  Serial.begin( _serialRate );
+  Serial.print( CHAR_ESC "E" );
+}
+
+void Tellymate::diagnostic()
+{
+  Serial.print( CHAR_ESC "Q");
+}
+
+void Tellymate::clearscreen()
+{ // <ESC>E
+  Serial.print( "\x1B" "E" ); 
+}
+
+void Tellymate::cursoron()
+{ // <ESC>e 
+  Serial.print( CHAR_ESC "e" );
+}
+
+void Tellymate::cursoroff()
+{ // <ESC>f 
+  Serial.print( CHAR_ESC "f" );
+}
+
+void Tellymate::printchar( unsigned char char2print )
+{
+  Serial.print( char2print );
+}
+
+void Tellymate::cursorto( uint8_t row , uint8_t col )
+{ // <ESC>Yrc
+  Serial.print( CHAR_ESC "Y" ) ;
+  Serial.print((unsigned char)(32 + row)) ;
+  Serial.print((unsigned char)(32 + col)) ;
+}
---
+++ Tellymate.h
@@ -0,0 +1,58 @@
+/*
+  Tellymate.h - Arduino Library for using the Tellymate Shield
+  Created by goldfish yo! 
+  Copyright (c) 2009, goldfish yo!
+  
+  Portions based on Tellymate demo code and specifications
+  Created and Copyright (c) 2009, Nigel Batten.
+  http://www.batsocks.co.uk/
+  
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+	1.	Redistributions of source code must retain the above copyright
+		notice, this list of conditions and the following disclaimer.
+	2.	Redistributions in binary form must reproduce the above copyright
+		notice, this list of conditions and the following disclaimer in the
+		documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+
+===============================================
+
+*/
+
+#ifndef Tellymate_h
+#define Tellymate_h
+
+#define CHAR_ESC "\x1B"
+
+//#include "WProgram.h"
+#include <inttypes.h>
+
+class Tellymate
+{
+  public:
+    Tellymate( int );
+    void init( long );
+    void diagnostic();
+    void clearscreen();
+    void cursoron();
+    void cursoroff();
+    void printchar( unsigned char );
+    void cursorto( uint8_t, uint8_t );
+  private:
+    long _serialRate;
+};
+
+#endif
